a7ae8881570d87f245e4083c8a886f8f900a2f83
NeXFP <giovannycardoso50@gmail.com>
2021-10-17 20:11:31 -0400

    Added files and worked on adding array of questions

---
+++ index.js
@@ -0,0 +1,24 @@
+// TODO: Include packages needed for this application
+
+// TODO: Create an array of questions for user input
+const questions = [
+    {
+        type: "input",
+        name: "title",
+        message: "What is the title for the current project?"
+    },
+    {
+        type: "input",
+        name: "description",
+        message: "Here you will write a basic description of the project:",
+    }
+];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
