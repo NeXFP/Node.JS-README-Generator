02ab75dd9cbfe50ca3a07755e4d597210a0d8227
NeXFP <giovannycardoso50@gmail.com>
2021-10-23 03:35:08 -0400

    Changed badges up and adjusted quotation to match rest of code

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,29 +1,30 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
-  if (license === 'MIT') {
-    return `https://img.shields.io/badge/license-MIT-blue`
-  } else if (license === 'APACHE 2.0') {
-    return `https://img.shields.io/badge/license-APACHE%202.0-blue`
-  } else if (license === 'GPL 3.0') {
-    return `https://img.shields.io/badge/license-GPL%203.0-blue`
+  if (license === "MIT") {
+    return `https://img.shields.io/badge/License-MIT-yellow`
+  } else if (license === "APACHE 2.0") {
+    return `https://img.shields.io/badge/License-Apache%202.0-yellow`
+  } else if (license === "GPL 3.0") {
+    return `https://img.shields.io/badge/License-GPL%203.0-yellow`
   } else {
-    return " ";
+    return "https://img.shields.io/badge/License-NONE-yellow";
   }
 }
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
 function renderLicenseLink(license) {
-  if (license === 'MIT') {
+  if (license === "MIT") {
     return `https://opensource.org/licenses/MIT`
-  } else if (license === 'APACHE 2.0') {
+  } else if (license === "APACHE 2.0") {
     return `https://opensource.org/licenses/Apache-2.0`
-  } else if (license === 'GPL 3.0') {
+  } else if (license === "GPL 3.0") {
     return `https://opensource.org/licenses/GPL-3.0`
   } else {
     return " ";
   }
 }
+
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {}
